bcad21eae80866f386ab333654c44e99d3a05180
Santiago Alexis Valencia Zapata <svalenciaz@unal.edu.co>
2021-05-02 01:00:36 -0500

    Product Info, Buy Product and Sells In Country
    Allows buy products with restrictions, and also get information about products and sell in each country. Events emited.

--- ContratoVanessaDaou.sol
+++ ContratoVanessaDaou.sol
@@ -3,19 +3,28 @@ pragma solidity >0.8.0;
 
 contract VanessaDaou {
     
-    // 
+    // Global variables
     
     address payable private owner;
     
     uint8 private deleteTries;
     
+    uint private totalDebts;
+    
+    // Constructor
     
     constructor(){
         
         owner = payable(msg.sender);
+        
+        // It will decrease each time the owner try to destruct the contract
         deleteTries = 3;
         
+        totalDebts = 0;
+        
     }
+    
+    // Structures
     
     struct Product{
         string name;
@@ -23,6 +32,8 @@ contract VanessaDaou {
         uint price;
         string description;
     }
+    
+    // Each Client has its own debts and total expend
     
     struct Client{
         address clientAddress;
@@ -33,32 +44,43 @@ contract VanessaDaou {
         uint debt;
     }
     
-    // TODO revisar si debe ser public o private
+    // Mappings
     
-    mapping (string => Product) public products;
+    // First two save products and clients to search by its name and code respectively
     
-    mapping (uint => Client) public clients;
+    mapping (string => Product) private products;
     
-    mapping (address => uint) public codeClientByAddress;
+    mapping (uint => Client) private clients;
     
-    mapping (string => bool) private usedProductNames;
+    // Next one maps user addresses with its unique code (used in map clients)
+    
+    mapping (address => uint) private clientCodeByAddress;
+    
+    // Next two map the names and codes used by the products and clients respectively to avoid duplication
+    
+    mapping (string => bool) private productNames;
     
     mapping (uint => bool) private usedClientCodes;
     
-    mapping (string => uint) public expendByCountries;
+    // This one saves the total Ether expend by the client of each country, key string is the country name
+    
+    mapping (string => uint) private expendByCountries;
+    
+    
+    // Modifiers
     
     modifier isOwner(){
         require(msg.sender == owner, "Only the owner can call this");
         _;
     }
     
-    modifier isPositive (uint number) {
-        require (number>0, "Arguments need to be non-negative");
+    modifier isNotClient () {
+        require (clientCodeByAddress[msg.sender]==0, "This Address is already registered");
         _;
     }
     
-    modifier productNotExists (string memory _name) {
-        require (!usedProductNames[_name], "Name is in use");
+    modifier isClient () {
+        require (clientCodeByAddress[msg.sender]!=0, "User not registered as client");
         _;
     }
     
@@ -67,10 +89,57 @@ contract VanessaDaou {
         _;
     }
     
-    modifier clientAddressAlreadyRegistered (address _address) {
-        require (codeClientByAddress[_address]==0, "This Address is already registered");
+    modifier hasNotDebt (){
+        uint code = clientCodeByAddress[msg.sender];
+        require (clients[code].debt == 0, "Client with debt can not buy");
         _;
     }
+    
+    modifier isPositive (uint number) {
+        require (number>0, "Arguments need to be positive");
+        _;
+    }
+    
+    modifier productNotExists (string memory _name) {
+        require (!productNames[_name], "Product name already in use");
+        _;
+    }
+    
+    modifier productAvailable (string memory _name) {
+        require (products[_name].units > 0, "Product is not available");
+        _;
+    }
+    
+    modifier productExists (string memory _name) {
+        require (productNames[_name], "Product doesn't exists");
+        _;
+    }
+    
+    /*
+    Prices are stored in Ether value, but by default systems assumes they are weis
+    so we need to make the conversion from Ethers to Weis multipling by 10^18
+    */
+    modifier exactPrice (uint price) {
+        require (msg.value == (price*10**18), "Just can pay with the exact price in Ether");
+        _;
+    }
+    
+    // Events
+    
+    event CreateNewProduct (address creator, string name, uint units, uint price, string description);
+    
+    event CreateNewClient (address client, uint code, string name, string country);
+    
+    event BuyProduct (address seller, address buyer, string produtcName, uint clientCode, uint basePrice, uint price);
+    
+    // Functions
+    
+    /*
+    newProduct creates a new product if it is called by the owner, if the product name doesn't exists yet,
+    and if its numeric arguments are entire positive numbers
+    
+    It also updates the produtcNames map with the new product name to true
+    */
     
     function newProduct(string memory _name, uint256 _units, uint256 _price, string memory _description)
     public
@@ -87,15 +156,25 @@ contract VanessaDaou {
         product.description = _description;
         
         products[_name] = product;
-        usedProductNames[_name] = true;
+        productNames[_name] = true;
+        
+        emit CreateNewProduct(msg.sender, _name, _units, _price, _description);
         
     }
     
+    /*
+    newClient creates a new client if the user has not already a client binded to its address,
+    if its code is positive and if the client code doesn't exists yet
+    
+    Initialize the client's totalExpend and debt in 0, and updates the clientCodeByAddress and
+    usedClientCodes maps with a address to code (uint) and code (uint) to true respectively
+    */
+    
     function newClient (uint _code, string memory _name, string memory _country)
     public
-    clientCodeNotExists(_code)
+    isNotClient
     isPositive(_code)
-    clientAddressAlreadyRegistered(msg.sender)
+    clientCodeNotExists(_code)
     {
         Client memory client;
         
@@ -106,23 +185,127 @@ contract VanessaDaou {
         client.debt = 0;
         client.totalExpend = 0;
         
-        codeClientByAddress[msg.sender] = _code;
+        clientCodeByAddress[msg.sender] = _code;
         clients[_code] = client;
         usedClientCodes[_code] = true;
         
+        emit CreateNewClient(msg.sender, _code, _name, _country);
+        
     }
     
+    /*
+    finalPrice show to the client the product price with the given name while it exists
+    calculates for each client and each product if it deserves a discount
+    */
     
+    function finalPrice (string memory productName)
+    public
+    view
+    isClient
+    productExists(productName)
+    returns (uint)
+    {
+        uint clientCode = clientCodeByAddress[msg.sender];
+        uint clientExpend = clients[clientCode].totalExpend;
+        uint productPrice = products[productName].price;
+        
+        if ((productPrice>=3) && (clientExpend>=50)){
+            return (productPrice - 3);
+        }
+        else{
+            return (productPrice);
+        }
+    }
+    
+    /*
+    productInformation shows to the clients all the information about a product
+    referenced with a given name while it exists
+    */
+    
+    function productInformation (string memory productName)
+    public
+    view
+    isClient
+    productExists(productName)
+    returns (string memory name, uint units, uint price, string memory description)
+    {
+        Product memory product = products[productName];
+        name = product.name;
+        units = product.units;
+        price = product.price;
+        description = product.description;
+    }
+    
+    /*
+    buyProduct allows clients to pay for a product while exists one with the given name
+    and it has units in inventary, but just to clients thas has not debts and pays the exact
+    product price
+    
+    The function updates client totalExpend with the value given by the function finalPrice
+    and the product unit with the same value minus 1.
+    
+    It also updates the expendByCountries map usind the client country's name as key and adding to
+    its remaining value, the current buy payment price
+    */
+    
+    function buyProduct (string memory productName)
+    public
+    payable
+    isClient
+    productExists(productName)
+    productAvailable(productName)
+    hasNotDebt
+    exactPrice(finalPrice(productName))
+    {
+        uint clientCode = clientCodeByAddress[msg.sender];
+        Client memory client = clients[clientCode];
+        Product memory product = products[productName];
+        
+        uint buyPrice = finalPrice(productName);
+        
+        client.totalExpend += buyPrice;
+        product.units -= 1;
+        
+        clients[clientCode] = client;
+        products[productName] = product;
+        expendByCountries[client.country] += buyPrice;
+        
+        emit BuyProduct (owner, msg.sender, product.name, client.code, product.price, buyPrice);
+    }
+    
+    /*
+    sellsInCountry returns to the owner the amount sells of a country by the given name
+    */
+    
+    function sellsInCountry (string memory countryName)
+    public
+    view
+    isOwner
+    returns (uint countryTotal)
+    {
+        countryTotal = expendByCountries[countryName];
+    }
+    
+    /*
+    destructContract allows the owner to destruct the contract after 3 function uses
+    
+    If the function haven't been used 3 times, it returns a warning message
+    */
     
     function destructContract()
     public
     payable
     isOwner
+    returns (string memory message)
     {
+        message = "";
         deleteTries -= 1;
         if (deleteTries == 0){
             selfdestruct(owner);
-            deleteTries = 3;
+        } else if (deleteTries == 2) {
+            message = "Are you sure? Press de button other 2 times";
+        } else {
+            message = "Are you sure? Press de button 1 more time";
         }
     }
     
